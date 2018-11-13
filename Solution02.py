89a35a5dd584254a31268b70f312630b23de5612
Gonz8088 <43080353+Gonz8088@users.noreply.github.com>
2018-11-13 17:00:08 -0600

    Update Solution02.py

--- Solution02.py
+++ Solution02.py
@@ -1,4 +1,18 @@
+# Paul Gonzales
+# ITEC2313
+# 2018-11-13
+
+def wins(team, f_handle):
+  #TODO determine number of instances of team in f_handle
+  return()
+
 def main():
+  fh = openFile("WorldSeriesWinners.txt", 'r')
+  
+  tm = input("Enter Team Name: ")
+  
+  wins(tm, fh)
+  
   return()
 
 main()
